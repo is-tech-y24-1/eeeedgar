525eebfd7dd8fac23a9f8e2d45905e4478aeecc6
eeeedgar <oakmillennial@gmail.com>
2022-05-26 20:17:21 +0300

    feat: add Task I7 Analyzers

---
+++ lab-3/AnalyzerTemplate/AnalyzerTemplate/AnalyzerI7List.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using static Microsoft.CodeAnalysis.CSharp.SyntaxKind;
+
+namespace AnalyzerTemplate
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AnalyzerI7List : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "AnalyzerI7List";
+        
+        private static readonly LocalizableString Title = new LocalizableResourceString(
+            nameof(Resources.AnalyzerI7Title),
+            Resources.ResourceManager,
+            typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(
+            nameof(Resources.AnalyzerI7MessageFormat),
+            Resources.ResourceManager,
+            typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(
+            nameof(Resources.AnalyzerI7Description),
+            Resources.ResourceManager,
+            typeof(Resources));
+        private const string Category = "Refactoring";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            
+            context.RegisterSyntaxNodeAction(AnalyzeNode, ReturnStatement);
+        }
+
+        private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var returnStatement = (ReturnStatementSyntax) context.Node;
+
+            if (returnStatement.Expression is null)
+                return;
+            
+            if (!returnStatement.Expression.IsKind(NullLiteralExpression))
+                return;
+            
+            
+            var method = returnStatement.Ancestors().OfType<MethodDeclarationSyntax>().FirstOrDefault();
+            if (method is null)
+                return;
+            
+            var genericType = method.ReturnType.DescendantNodesAndSelf().OfType<GenericNameSyntax>().FirstOrDefault();
+            if (genericType is null)
+                return;
+            
+            if (!genericType.Identifier.ValueText.Equals("List")
+                && !genericType.Identifier.ValueText.Equals("ICollection"))
+                return;
+
+            var diagnostic = Diagnostic.Create(Rule, returnStatement.Expression.GetLocation());
+            context.ReportDiagnostic(diagnostic);
+        }
+    }
+}
