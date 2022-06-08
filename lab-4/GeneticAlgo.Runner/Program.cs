28a5fa3ce8e6c2024c78a831cfd6fbab8292335f
eeeedgar <oakmillennial@gmail.com>
2022-06-08 16:09:02 +0300

    feat: add tracing run

---
+++ lab-4/GeneticAlgo.Runner/Program.cs
@@ -0,0 +1,19 @@
+﻿using GeneticAlgo.Shared.Models;
+using GeneticAlgo.Shared.Tools;
+
+var pointCount = 50000;
+var maximumValue = 10;
+var circleCount = 5;
+var iterationLimit = 1000;
+var fitnessAccuracy = 0.05;
+var partOfBadElementsToReplace = 0.1;
+
+var executionContext = new SolutionFindingExecutionContext(pointCount, maximumValue, circleCount, iterationLimit, fitnessAccuracy, partOfBadElementsToReplace);
+
+
+var result = IterationResult.IterationFinished;
+var iterationCounter = 0;
+while (result != IterationResult.SolutionFound && iterationCounter++ < iterationLimit)
+    result = await executionContext.ExecuteIterationAsync();
+    
+Console.WriteLine(result);
