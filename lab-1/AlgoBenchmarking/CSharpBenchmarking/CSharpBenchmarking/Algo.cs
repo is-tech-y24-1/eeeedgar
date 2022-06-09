72d913b5c7666ba1450a1596f0f1d53abb93eeaa
eeeedgar <oakmillennial@gmail.com>
2022-06-09 06:04:05 +0300

    feat: add cs sort algo benchmarking

---
+++ lab-1/AlgoBenchmarking/CSharpBenchmarking/CSharpBenchmarking/Algo.cs
@@ -0,0 +1,97 @@
+﻿namespace CSharpBenchmarking
+{
+    public class Algo
+    {
+        public static void QuickSort(IList<int> arr, int left, int right)
+        {
+            if (left >= right)
+                return;
+
+            var pivot = Partition(arr, left, right);
+
+            if (pivot > 1)
+            {
+                QuickSort(arr, left, pivot - 1);
+            }
+
+            if (pivot + 1 < right)
+            {
+                QuickSort(arr, pivot + 1, right);
+            }
+        }
+
+        public static void MergeSort(IList<int> arr, int left, int right)
+        {
+            if (right <= left)
+                return;
+            
+            var mid = (right + left) / 2;
+            
+            MergeSort(arr, left, mid);
+            MergeSort(arr, (mid + 1), right);
+            
+            Merge(arr, left, (mid + 1), right);
+        }
+
+        private static void Merge(IList<int> arr, int left, int mid, int right)
+        {
+            int[] buf = new int[arr.Count];
+            int i;
+            var eol = (mid - 1);
+            var pos = left;
+            var num = (right - left + 1);
+
+            while ((left <= eol) && (mid <= right))
+            {
+                if (arr[left] <= arr[mid])
+                {
+                    buf[pos++] = arr[left++];
+                }
+                else
+                {
+                    buf[pos++] = arr[mid++];
+                }
+            }
+
+            while (left <= eol)
+                buf[pos++] = arr[left++];
+ 
+            while (mid <= right)
+                buf[pos++] = arr[mid++];
+            
+            for (i = 0; i < num; i++)
+            {
+                arr[right] = buf[right];
+                right--;
+            }
+        }
+
+        private static int Partition(IList<int> arr, int left, int right)
+        {
+            var pivot = arr[left];
+            while (true)
+            {
+                while (arr[left] < pivot)
+                {
+                    left++;
+                }
+
+                while (arr[right] > pivot)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    if (arr[left] == arr[right])
+                        return right;
+                    (arr[right], arr[left]) = (arr[left], arr[right]);
+                }
+                else
+                {
+                    return right;
+                }
+            }
+        }
+    }
+}
---
+++ lab-1/AlgoBenchmarking/CSharpBenchmarking/CSharpBenchmarking/Benchmarking.cs
@@ -0,0 +1,37 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace CSharpBenchmarking
+{
+    public class Benchmarking
+    {
+        [Params(100, 1000, 10000, 100000)]
+        public int Length;
+
+        private int[] _array;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _array = new int[Length];
+        }
+
+        [IterationSetup]
+        public void IterationSetup()
+        {
+            var random = new Random();
+            for (var i = 0; i < Length; i++)
+            {
+                _array[i] = random.Next();
+            }
+        }
+
+        [Benchmark]
+        public void QuickSort() => Algo.QuickSort(_array, 0, Length - 1);
+            
+        [Benchmark]
+        public void MergeSort() => Algo.MergeSort(_array, 0, Length - 1);
+
+        [Benchmark]
+        public void DefaultSort() => Array.Sort(_array);
+    }
+}
