4fd23b4678c3f77634e2aa074430788e8a9a7af9
eeeedgar <oakmillennial@gmail.com>
2022-06-09 05:42:54 +0300

    feat: create cs graph algo package

---
+++ lab-1/GraphAlgo/CSharp/EeeeGraphAlgo/EeeeGraphAlgo/Algos.cs
@@ -0,0 +1,35 @@
+﻿namespace EeeeGraphAlgo
+{
+    public class Algos
+    {
+        public static void BFS(Node start)
+        {
+            var queue = new List<Node>();
+            var next = new List<Node>();
+            
+            queue.Add(start);
+
+            while (queue.Count > 0)
+            {
+                foreach (var node in queue)
+                {
+                    node.IsVisited = true;
+                    next.AddRange(node.Children.Where(child => !child.IsVisited));
+                }
+
+                queue = next;
+                next = new List<Node>();
+            }
+        }
+
+        public static void DFS(Node start)
+        {
+            start.IsVisited = true;
+
+            foreach (var child in start.Children.Where(child => !child.IsVisited))
+            {
+                DFS(child);
+            }
+        }
+    }
+}
